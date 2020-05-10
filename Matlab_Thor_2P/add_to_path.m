8cc8a45a9c2becac3d4895b9ec0dcc0b02a6d1ae
samsoon-inayat <samsoon.inayat@gmail.com>
2020-05-09 23:40:48 -0600

    file shuffling

--- Matlab_Thor_2P/add_to_path.m
+++ Matlab_Thor_2P/add_to_path.m
@@ -9,13 +9,16 @@ temp = fullfile(mainDir,'MatlabCode','Common');addpath(temp);
 temp = fullfile(mainDir,'Downloaded','append_pdfs');addpath(temp);
 temp = fullfile(mainDir,'MatlabCode','Thor2P');addpath(temp);
 temp = fullfile(mainDir,'MatlabCode','FigureFunctions');addpath(temp);
+temp = fullfile(mainDir,'Downloaded','spatial-information-metrics-master');addpath(temp);
+temp = fullfile(mainDir,'Downloaded','npy-matlab-master');addpath(temp);
+temp = fullfile(mainDir,'Downloaded');addpath(temp);
 temp = fullfile(mainDir,'Downloaded','ca_source_extraction-master');p = genpath(temp);addpath(p);
+
 
 temp = fullfile(mainDir,'Downloaded','Suite2P-master');p = genpath(temp);addpath(p);
 
 temp = pwd; p = genpath(temp);addpath(p);
 remove_from_path
-% addToPath(matlabCodeFolder);
-% rmpath(scriptPath);
+
 
 
