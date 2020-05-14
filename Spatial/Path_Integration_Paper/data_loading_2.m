af9fa2aa0e879480e170566d9113d04e647326d9
samsoon-inayat <samsoon.inayat@gmail.com>
2020-05-14 14:48:11 -0600

    wok

--- Spatial/Path_Integration_Paper/data_loading_2.m
+++ Spatial/Path_Integration_Paper/data_loading_2.m
@@ -10,7 +10,7 @@ ei10 = loadContextsResponses(ei10,[1 1],[0 1 0]);
 
 selT = T10.T([7 11 13 14 16 18 20 21],:);
 ei10 = getData_py(f,selT);
-owr = [1,1]; owrp = [1 0 0];
+owr = [1,1]; owrp = [0 0 0];
 for ii = 1:size(selT,1)
     if ismember(ii,[1:8])
         ei10(ii) = loadContextsResponses(ei10(ii),owr,owrp);
--- Spatial/add_to_path.m
+++ Spatial/add_to_path.m
@@ -1,14 +1,15 @@
 function add_to_path
 currentDir = pwd;
 cd('..');
+interDir = pwd;
 cd('..');
 mainDir = pwd;
 cd(currentDir);
 % addpath(scriptPath);
-temp = fullfile(currentDir,'MatlabCode','Common');addpath(temp);
+temp = fullfile(interDir,'MatlabCode','Common');addpath(temp);
 temp = fullfile(mainDir,'Downloaded','append_pdfs');addpath(temp);
-temp = fullfile(currentDir,'MatlabCode','Thor2P');addpath(temp);
-temp = fullfile(currentDir,'MatlabCode','FigureFunctions');addpath(temp);
+temp = fullfile(interDir,'MatlabCode','Thor2P');addpath(temp);
+temp = fullfile(interDir,'MatlabCode','FigureFunctions');addpath(temp);
 temp = fullfile(mainDir,'Downloaded','spatial-information-metrics-master');addpath(temp);
 temp = fullfile(mainDir,'Downloaded','npy-matlab-master');addpath(temp);
 temp = fullfile(mainDir,'Downloaded');addpath(temp);
