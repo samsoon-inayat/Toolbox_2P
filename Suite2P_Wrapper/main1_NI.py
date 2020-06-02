5f168dc24c88ef0a0b7c0be0b700450a893a7b97
Samsoon Inayat <samsoon.inayat@gmail.com>
2020-06-02 01:15:42 -0600

    re_processing_data

--- Suite2P_Wrapper/main1_NI.py
+++ Suite2P_Wrapper/main1_NI.py
@@ -17,8 +17,8 @@ tif_data_folder = 'E:/Users/samsoon.inayat/S_Drive/Tif_Data'
 # processed_data_folder where the results of suite 2p will be stored
 #processed_data_folder = 'E:/Users/samsoon.inayat/OneDrive - University of Lethbridge/pySuite2P/processed_data'
 processed_data_folder = 'E:/Users/samsoon.inayat/S_Drive/Processed_Data'
-nas_processed_data_folder = 'Y:/homes/samsoon.inayat/S_Drive/pySuite2p_Processed_Data'
-# nas_processed_data_folder = 'Z:/homes/brendan.mcallister/2P/Processed_Data'
+nas_processed_data_folder = 'Z:/homes/brendan.mcallister/2P/Processed_Data'
+
 #E:\Users\samsoon.inayat\S_Drive\Processed_Data
 # Directory name where raw data is
 #dir_name = '//mohajerani-nas.uleth.ca/storage/homes/samsoon.inayat/Data/183628/2019-07-01/1_003'
