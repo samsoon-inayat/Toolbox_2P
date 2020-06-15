6fc39c7182f4fa68102204ae787a02c7fc500a74
samsoon-inayat <samsoon.inayat@gmail.com>
2020-06-15 11:34:21 -0600

    -

--- Spatial/Path_Integration_Paper/figure_cell_seq.m
+++ Spatial/Path_Integration_Paper/figure_cell_seq.m
@@ -97,17 +97,67 @@ end
 all_mean_over_cells(:,:,ani) = mean_over_cells;
 end
 n = 0;
+result = descriptiveStatistics(all_mean_over_cells,3)
 %%
 figure(101);clf;hold on;
 for ii = 1:crt
-    plot(1:9,mean_over_cells(ii,:),'color',colors{ii});
+%     plot(1:9,result.avg(ii,:),'color',colors{ii});
+    errorbar(1:9,result.avg(ii,:),result.sem(ii,:),'color',colors{ii});
 %     subplot(1,crt,ii);
 %     imagesc(all_cell_pos{ii});
 end
-return;
-sigRM = significanceTesting(mean_over_cells');
+% return;
+% sigRM = significanceTesting(mean_over_cells');
 
 n = 0;
+
+%%
+% perform repeated measures anova
+
+    ind = 1;
+    for ii = 1:4
+        for jj = 1:9
+            varNames{ind} = sprintf('C%dTD%d',ii,jj);
+            ind = ind + 1;
+        end
+    end
+    for ii = 1:size(all_mean_over_cells,3)
+        thisd = all_mean_over_cells(:,:,ii);
+        data(ii,:) = reshape(thisd',1,36);
+    end
+    colVar1 = [ones(1,9) 2*ones(1,9) 3*ones(1,9) 4*ones(1,9)];    colVar2 = [1:9 1:9 1:9 1:9];
+    within = table(colVar1',colVar2'); within.Properties.VariableNames = {'Condition','TrialDiff'};
+    ra = repeatedMeasuresAnova(data,varNames,within);
+    rm = ra.rm;
+    mcTI = find_sig_mctbl(multcompare(rm,'TrialDiff','By','Condition','ComparisonType','bonferroni'),6);
+    mcConds = find_sig_mctbl(multcompare(rm,'Condition','By','TrialDiff','ComparisonType','bonferroni'),6);
+    [h,p] = populate_multcomp_h_p(data,mcTI,mcConds);
+    [mVar semVar] = findMeanAndStandardError(data);
+    ds = descriptiveStatistics(data)
+%%
+    n = 0;
+    xdata = [1:1.5:(10*size(data,2))]; xdata = xdata(1:size(data,2)); maxY = 45;
+    hf = figure(15);clf;set(gcf,'Units','Inches');set(gcf,'Position',[3 3 4 1.5],'color','w');
+    hold on;
+    ind = 1;
+    for ii = 1:4
+        for jj = 1:9
+            tcolors{ind} = colors{ii};
+            ind = ind + 1;
+        end
+    end
+    hbs = plotBarsWithSigLines(mVar,semVar,combs,[h p],'colors',tcolors,'sigColor','k',...
+        'maxY',maxY,'ySpacing',1.25,'sigTestName','','sigLineWidth',0.25,'BaseValue',0.1,...
+        'xdata',xdata,'sigFontSize',7,'sigAsteriskFontSize',8,'barWidth',0.7,'sigLinesStartYFactor',0);
+    set(gca,'xlim',[0.25 max(xdata)+.75],'ylim',[25 maxY],'FontSize',6,'FontWeight','Bold','TickDir','out');
+    xticks = xdata; 
+    xticklabels = {'T1','T2','T3','T4','T5','T6','T7','T8','T9'};xticklabels = repmat(xticklabels,1,4);
+    set(gca,'xtick',xticks,'xticklabels',xticklabels);
+    xtickangle(30);
+    changePosition(gca,[0.1 0.02 -0.03 -0.011])
+    put_axes_labels(gca,{[],[0 0 0]},{{'Percent cell_seq _shift','(z-score)'},[0 0 0]});
+    save_pdf(hf,mData.pdf_folder,sprintf('cell_seq'),600);
+    
 
 
 %%
