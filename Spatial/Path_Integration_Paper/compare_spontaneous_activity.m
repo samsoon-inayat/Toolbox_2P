a6316f757e9530a3b0c2eb39fd3cfb38beb32f99
Samsoon Inayat <samsoon.inayat@gmail.com>
2020-10-06 22:00:28 -0600

    -

--- Spatial/Path_Integration_Paper/compare_spontaneous_activity.m
+++ Spatial/Path_Integration_Paper/compare_spontaneous_activity.m
@@ -17,7 +17,7 @@ out_A = get_spike_rate(ei_A);
 
 n=0;
 %%
-if 1
+if 0
     data_C = out_C.m_sp_animal_th;
     data_A = out_A.m_sp_animal_th;
     data = [data_C' data_A'];
@@ -26,10 +26,10 @@ if 1
     incr = (maxBin-minBin)/50;
     hf = figure(1002);clf;set(gcf,'Units','Inches');set(gcf,'Position',[5 6 2 1.5],'color','w');
     hold on;
-    [ha,hb,hca,sigR] = plotDistributions(data,'colors',colors,'maxY',90,'cumPos',[0.5 0.26 0.25 0.5],'min',minBin,'incr',incr,'max',maxBin);
+    [ha,hb,hca,sigR] = plotDistributions(data,'colors',colors,'maxY',200,'cumPos',[0.5 0.26 0.25 0.5],'min',minBin,'incr',incr,'max',maxBin);
     hold on;
     legs = {'Ctrl','AD'};
-    ylim([0 15]);
+%     ylim([0 15]);
     xlim([0 maxBin+10])
     xlims = xlim; dx = xlims(2) - xlims(1); ylims = ylim; dy = ylims(2) - ylims(1);
     legs{length(legs)+1} = [xlims(1)+dx/1.5 dx/30 ylims(1)+dy/3 dy/15];
@@ -38,10 +38,72 @@ if 1
     h = xlabel('Spike Rate');%changePosition(h,[0 -dy/3 0]);
     h = ylabel('Percentage');changePosition(h,[-0.1 0 0]);
     set(gca,'FontSize',axes_font_size,'FontWeight','Bold');changePosition(ha,[0.07 0.01 -0.05 -0.05]);
-    file_name = fullfile(mData.pdf_folder,sprintf('%s_distribution spike rate',mfilename));
+    file_name = sprintf('%s_distribution spike rate.pdf',mfilename);
     save_pdf(hf,mData.pdf_folder,file_name,600);
 end
 
+%%
+if 1
+    [mVar_C,semVar_C] = findMeanAndStandardError(out_C.m_sp_animal_level_th);
+    [mVar_A,semVar_A] = findMeanAndStandardError(out_A.m_sp_animal_level_th);
+    mVar = [mVar_C,mVar_A];
+    semVar = [semVar_C,semVar_A];
+    [h,p,ci,stats] = ttest2(out_C.m_sp_animal_level_th,out_A.m_sp_animal_level_th)
+    combs = [1 2]; %p = ones(size(combs,1),1); h = logical(zeros(size(combs,1),1));
+    xdata = [1 2]; maxY = 40;
+    colors = mData.colors;
+    hf = figure(5);clf;set(gcf,'Units','Inches');set(gcf,'Position',[5 7 2.25 1],'color','w');
+    hold on;
+    tcolors = {colors{1};colors{1}};
+    hbs = plotBarsWithSigLines(mVar,semVar,combs,[h p],'colors',tcolors,'sigColor','k',...
+        'maxY',maxY,'ySpacing',10,'sigTestName','','sigLineWidth',0.25,'BaseValue',0.1,...
+        'xdata',xdata,'sigFontSize',7,'sigAsteriskFontSize',10,'barWidth',0.7,'sigLinesStartYFactor',0.1);
+    for ii = 2:2:length(hbs)
+        set(hbs(ii),'facecolor','none','edgecolor',tcolors{ii});
+    end
+    % plot([0.5 11],[-0.5 0.5],'linewidth',1.5)
+    set(gca,'xlim',[0.25 xdata(end)+0.75],'ylim',[0 maxY+1],'FontSize',6,'FontWeight','Bold','TickDir','out');
+    xticks = xdata(1:end); xticklabels = {'Ctrl','AD'};
+    set(gca,'xtick',xticks,'xticklabels',xticklabels);
+    changePosition(gca,[0.02 0.03 0.02 -0.11]);
+    put_axes_labels(gca,{[],[0 0 0]},{'FR (Hz)',[0 0 0]});
+    save_pdf(hf,mData.pdf_folder,sprintf('%s_bargraph',mfilename),600);
+end
+
+%%
+if 1
+    colVar1 = [ones(1,5) 2*ones(1,5)];
+    betweenTable_C = table(out_C.m_sp_animal_level_motion',out_C.m_sp_animal_level_rest','VariableNames',{'Motion','Rest'});
+    betweenTable_A = table(out_A.m_sp_animal_level_motion',out_A.m_sp_animal_level_rest','VariableNames',{'Motion','Rest'});
+    betweenTable = [table(colVar1','VariableNames',{'Group'}) [betweenTable_C;betweenTable_A]];
+    betweenTable.Group = categorical(betweenTable.Group);
+    withinTable = table([1 2]','VariableNames',{'Type'});
+    withinTable.Type = categorical(withinTable.Type);
+    rmaR = repeatedMeasuresAnova(betweenTable,withinTable);
+%     findMeanAndStandardError
+    mVar = rmaR.est_marginal_means.Mean;
+    semVar = rmaR.est_marginal_means.StdErr;
+    combs = rmaR.combs;
+    p = rmaR.p; h = p<0.05;
+    xdata = [1 2 3 4]; maxY = 1;
+    colors = mData.colors;
+    hf = figure(5);clf;set(gcf,'Units','Inches');set(gcf,'Position',[5 7 2.25 1],'color','w');
+    hold on;
+    tcolors = {colors{1};colors{2};colors{1};colors{2}};
+    hbs = plotBarsWithSigLines(mVar,semVar,combs,[h p],'colors',tcolors,'sigColor','k',...
+        'maxY',maxY,'ySpacing',0.1,'sigTestName','','sigLineWidth',0.25,'BaseValue',0.1,...
+        'xdata',xdata,'sigFontSize',7,'sigAsteriskFontSize',10,'barWidth',0.7,'sigLinesStartYFactor',0.1);
+    for ii = 2:2:length(hbs)
+        set(hbs(ii),'facecolor','none','edgecolor',tcolors{ii});
+    end
+    % plot([0.5 11],[-0.5 0.5],'linewidth',1.5)
+    set(gca,'xlim',[0.25 xdata(end)+0.75],'ylim',[0 maxY+0],'FontSize',6,'FontWeight','Bold','TickDir','out');
+    xticks = xdata(1:end); xticklabels = {'Ctrl','AD'};
+    set(gca,'xtick',xticks,'xticklabels',xticklabels);
+    changePosition(gca,[0.02 0.03 0.02 -0.11]);
+    put_axes_labels(gca,{[],[0 0 0]},{'FR (Hz)',[0 0 0]});
+    save_pdf(hf,mData.pdf_folder,sprintf('%s_bargraph_overall',mfilename),600);
+end
 
 n = 0;
 
@@ -57,27 +119,29 @@ for ii = 1:length(ei_C)
     iscell = logical(ei.plane{1}.tP.iscell(:,1));
     spSigAll = spSigAll(iscell,:);
     speed = ei.b.fSpeed;
-    inds_motion = find(speed > 0);
-    inds_rest = find(speed == 0);
+    inds_motion = find(speed > 0); % find indices of array where speed is greater than zero
+    inds_rest = find(speed == 0); % rest period
 %     signal = spSigAll(1,:);
 %     figure(100);clf;plot(signal);hold on;plot(ones(size(signal))*(mean(signal)+3*std(signal)))
     frames_f = ei.plane{1}.b.frames_f;
-    [~,ind_frames_motion,~] = intersect(frames_f,inds_motion);
-    [~,ind_frames_rest,~] = intersect(frames_f,inds_rest);
-    m_sp_animal{ii} = nanmean(spSigAll,2);
-    m_sp_animal_motion{ii} = nanmean(spSigAll(:,ind_frames_motion),2);
-    m_sp_animal_rest{ii} = nanmean(spSigAll(:,ind_frames_rest),2);
-    allVals = [allVals;m_sp_animal{ii}];
-    allVals_motion = [allVals_motion;m_sp_animal_motion{ii}];
+    [~,ind_frames_motion,~] = intersect(frames_f,inds_motion); % frames where animal was moving
+    [~,ind_frames_rest,~] = intersect(frames_f,inds_rest); % frames where animal was at rest
+    m_sp_animal{ii} = nanmean(spSigAll,2); % mean spike rate 
+    m_sp_animal_motion{ii} = nanmean(spSigAll(:,ind_frames_motion),2); % mean spike rate when animal moving
+    m_sp_animal_rest{ii} = nanmean(spSigAll(:,ind_frames_rest),2); % mean spiek rate when animal at rest
+    allVals = [allVals;m_sp_animal{ii}]; % accumulating values over animals but for all cells
+    allVals_motion = [allVals_motion;m_sp_animal_motion{ii}]; % accumulating values over animals for all cells
     allVals_rest = [allVals_rest;m_sp_animal_rest{ii}];
-    m_sp_animal_level(ii) = nanmean(m_sp_animal{ii});
+    m_sp_animal_level(ii) = nanmean(m_sp_animal{ii}); % average over cells
     m_sp_animal_level_motion(ii) = nanmean(m_sp_animal_motion{ii});
     m_sp_animal_level_rest(ii) = nanmean(m_sp_animal_rest{ii});
-    thr = nanmean(spSigAll,2) + 3*nanstd(spSigAll,[],2);
+%     thr = nanmean(spSigAll,2) + 3*nanstd(spSigAll,[],2);
+    thr = 3*nanstd(spSigAll,[],2);
     for cn = 1:size(spSigAll,1)
         m_sp_animal_th{ii}(cn,1) = nanmean(spSigAll(cn,spSigAll(cn,:) > thr(cn)));
+%         figure(100);clf;plot(spSigAll(cn,:));hold on;plot(ones(size(spSigAll(cn,:)))*thr(cn))
     end
-    m_sp_animal_level_th(ii) = nanmean(m_sp_animal_th{ii});
+    m_sp_animal_level_th(ii) = nanmean(m_sp_animal_th{ii}); %finding mean over cells
     allVals_th = [allVals;m_sp_animal_th{ii}];
 end
 out.m_sp_animal = m_sp_animal;
