fbe18ce7ae2663f7fb132da5f11d2c83cb6c16df
Samsoon Inayat <samsoon.inayat@gmail.com>
2022-07-06 00:44:37 -0600

    -

--- Spatial/AD_Paper_Revamp/population_vectors.m
+++ Spatial/AD_Paper_Revamp/population_vectors.m
@@ -1,14 +1,13 @@
 %% population vector and correlation sensory
 while 1
-    ff = makeFigureRowsCols(107,[10 3 5 4],'RowsCols',[3 4],'spaceRowsCols',[0.01 -0.02],'rightUpShifts',[0 0],'widthHeightAdjustment',[10 -50]);
+    ff = makeFigureRowsCols(107,[10 3 3.5 2.7],'RowsCols',[3 4],'spaceRowsCols',[0.03 -0.01],'rightUpShifts',[0.1 0.11],'widthHeightAdjustment',[10 -80]);
 %     set(gcf,'color','w');    set(gcf,'Position',[10 3 3.5 3.75]);
     MY = 8; ysp = 1; mY = 0; % responsive cells
-    stp = 0.11; widths = [0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5]+0.2; gap = 0.16;
+    stp = 0.15; widths = [0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5]+0.18; gap = 0.16;
     adjust_axes(ff,[mY MY],stp,widths,gap,{'Cell #'});
     
     an = 4;
-    o = oC;
-    
+    o = oC;  
     si = [C1_t_D C2_t_D C3_t_D C4_t_D];
     Rs = o.Rs(:,si);mR = o.mR(:,si);
     ntrials = 70;
@@ -16,7 +15,7 @@ while 1
     good_FR = props1.good_zMI;
     good_FR = good_zMI_MFR_Gauss_C;
     [CRc,aCRc,mRR] = find_population_vector_corr(Rs,mR,good_FR,0);
-    for ii = 1:length(ff.h_axes)
+    for ii = 1:size(ff.h_axes,2)
         tR = Rs{an,ii};
         tmRR = mRR{an,ii};
         axes(ff.h_axes(1,ii));
@@ -30,6 +29,44 @@ while 1
         if ii == 1
             ylabel('Cell #');
         end
+        set(gca,'YTick',[],'XTick',[]);
+
+        ylims = ylim;
+        textstr = sprintf('%d',ceil(ylims(2)));
+        set_axes_top_text_no_line(ff.hf,gca,textstr,[0 -0.13 0 0]);
+        format_axes(gca);
+        
+        axes(ff.h_axes(2,ii));
+        tmRR = CRc{an,ii};
+        if ii < 5
+            xdata = [0 75 150];
+        else
+            xdata = [0 7.5 15];
+        end
+        ydata = [1 size(tmRR,1)];
+        imagesc(xdata,ydata,tmRR,[0 1]); set(gca,'Ydir','normal');
+        if ii == 1
+            ylabel('Distance (cm)');
+        end
+        set(gca,'YTick',[],'XTick',[]);
+
+        ylims = ylim;
+        textstr = sprintf('%d',ceil(ylims(2)));
+%         set_axes_top_text_no_line(ff.hf,gca,textstr,[0 -0.07 0 0]);
+        format_axes(gca);
+        
+        axes(ff.h_axes(3,ii));
+        tmRR = aCRc{ii};
+        if ii < 5
+            xdata = [0 75 150];
+        else
+            xdata = [0 7.5 15];
+        end
+        ydata = [1 size(tmRR,1)];
+        imagesc(xdata,ydata,tmRR,[0 1]); set(gca,'Ydir','normal');
+        if ii == 1
+            ylabel('Distance (cm)');
+        end
         set(gca,'YTick',[]);
         if ii < 5
             xlabel('Distance (cm)');
@@ -38,7 +75,7 @@ while 1
         end
         ylims = ylim;
         textstr = sprintf('%d',ceil(ylims(2)));
-        set_axes_top_text_no_line(ff.hf,gca,textstr,[0 -0.07 0 0]);
+%         set_axes_top_text_no_line(ff.hf,gca,textstr,[0 -0.07 0 0]);
         format_axes(gca);
     end
     save_pdf(ff.hf,mData.pdf_folder,sprintf('pop_vectors_%d.pdf',ntrials),600);
