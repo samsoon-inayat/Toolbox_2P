8117e17ec21e9461f62c64b9b5b8b092acf1cc91
Samsoon Inayat <samsoon.inayat@gmail.com>
2022-09-22 16:32:33 -0600

    -

--- Spatial/Revamp_distance_duration/bar_graphs_heere.m
+++ Spatial/Revamp_distance_duration/bar_graphs_heere.m
@@ -465,3 +465,28 @@ ylabel('dzMI (A.U.)');
 
 %% save file
 save_pdf(hf,mData.pdf_folder,sprintf('omnifile.pdf'),600);
+
+
+%% CT zMI
+ff = makeFigureRowsCols(107,[10 5 1.55 1],'RowsCols',[1 1],'spaceRowsCols',[0.01 -0.02],'rightUpShifts',[0.07 0.36],'widthHeightAdjustment',[10 -510]);
+  MY = 150; mY = 0; ys = 0.1;
+  stp = 0.27*magfac; widths = ([0.75 0.55 1.3 1.3 1.3 0.5 0.5 0.5]-0.05)*magfac; gap = 0.067*magfac;
+  adjust_axes(ff,[mY MY],stp,widths,gap,{''});
+  tcolors = repmat(mData.colors(1:2),1,6);
+  axes(ff.h_axes(1,1));
+    [xdata,mVar,semVar,combs,p,h,colors,xlabels] = get_vals_for_bar_graph_RMA(mData,ra,{'CT','bonferroni'},[1.5 1 1]);
+    xdata = make_xdata([2],[1 1.5]);
+%     hf = get_figure(5,[8 7 1.25 1]);
+    tcolors = repmat(mData.colors(1:9),2,1);
+    MmVar = max(mVar);
+    [hbs,maxY] = plotBarsWithSigLines(mVar,semVar,combs,[h p],'colors',tcolors,'sigColor','k',...
+        'ySpacing',ys,'sigTestName','','sigLineWidth',0.25,'BaseValue',0.01,...
+        'xdata',xdata,'sigFontSize',7,'sigAsteriskFontSize',8,'barWidth',0.5,'sigLinesStartYFactor',0.15);
+    set_axes_limits(gca,[0.35 xdata(end)+.65],[mY MY]); format_axes(gca);
+    xticks = xdata; xticklabels = {'Time','Dist'};
+    set(gca,'xtick',xticks,'xticklabels',xticklabels);% xtickangle(45)
+put_axes_labels(gca,{[],[0 0 0]},{'z-score',[0 0 0]});
+ht = set_axes_top_text_no_line(gcf,gca,'Mutual Information',[-0.05 -0.01 0.2 0]);set(ht,'FontWeight','Bold');
+% set_bar_graph_sub_xtick_text(ff.hf,gca,hbs,3,{'Air','No-Air'});
+format_axes(gca);
+save_pdf(ff.hf,mData.pdf_folder,'bar_graph.pdf',600);
