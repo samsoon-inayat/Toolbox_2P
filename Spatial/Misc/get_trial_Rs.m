08e213c405a5cb8ec2051eebc1486fe30407f0a2
Samsoon Inayat <samsoon.inayat@gmail.com>
2023-05-23 19:42:17 -0600

    -

--- Spatial/Misc/get_trial_Rs.m
+++ Spatial/Misc/get_trial_Rs.m
@@ -1,14 +1,14 @@
-function [allRsC,allmRsT] = get_trial_Rs(o,si,trialNs)
+function [allRsC,allmRsT,allmRsT_Raw] = get_trial_Rs(o,si,trialNs)
 
 Rs = o.Rs(:,si);mR = o.mR(:,si); 
 for cn = 1:length(si)
     trials = mat2cell([trialNs]',ones(size([trialNs]')));
     RsC = repmat(Rs(:,cn),1,length(trialNs));
-    mRsCT = cell(size(RsC,1),length(trials));
+    mRsCT = cell(size(RsC,1),length(trials)); mRsCT_Raw = mRsCT;
     for ii = 1:length(trials)
         ii;
-        [mRsCT(:,ii),~] = calc_mean_rasters(RsC(:,1),trials{ii});
+        [mRsCT(:,ii),~,mRsCT_Raw(:,ii)] = calc_mean_rasters(RsC(:,1),trials{ii});
     end
-    allmRsT{cn} = mRsCT;
-    allRsC{cn} = RsC;
+    allmRsT{cn} = mRsCT; allmRsT_Raw{cn} = mRsCT_Raw;
+     allRsC{cn} = RsC;
 end
